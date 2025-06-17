8b55e26d6793c41aa21c893c63a95b71605ddf7b
Raashi25 <raashihl25@gmail.com>
2025-06-17 14:31:27 +0530

    sop

---
+++ Samsung.java
@@ -0,0 +1,11 @@
+public class Samsung {
+    public static void main(String[] args) {
+        String productName = "Samsung Galaxy S24";
+        String description = "Flagship Samsung smartphone with AMOLED display and Snapdragon processor.";
+        double price = 899.99;
+
+        System.out.println("Product Name: " + productName);
+        System.out.println("Description: " + description);
+        System.out.println("Price: $" + price);
+    }
+}
